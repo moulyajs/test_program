df251aa60b39f9b3732437d9cb09e2a89f1d45b5
Moulya <moulya.anjanappa@gmail.com>
2024-02-15 20:03:18 +0530

    first commit

---
+++ binary.c
@@ -0,0 +1,119 @@
+/*
+#include <stdio.h>
+int main()
+{
+    int n, pos;
+    printf("enter the number");
+    scanf("%d", &n);
+    printf("enter position");
+    scanf("%d", &pos);
+    printf("before setting the bit %d\n", n);
+    n = n | 1 << pos;
+    printf("after setting the bit %d\n", n);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int n, pos;
+    printf("enter the number");
+    scanf("%d", &n);
+    printf("enter position");
+    scanf("%d", &pos);
+    printf("before clearing the bit %d\n", n);
+    n = n & ~(1 << pos);
+    printf("after clearing the bit %d\n", n);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 20, b = 40;
+    (a > b) ? printf("%d", a) : printf("%d", b);
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 100, b = 40;
+    char c = 'a';
+    printf("the size of a is %d\n", sizeof(a));
+    printf("the size of c is %d\n", sizeof(c));
+    printf("the size of float is %d\n", sizeof(float));
+    printf("the size of double is %d\n", sizeof(double));
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 11;
+    b = a++;
+    printf("the value of b %d\n", b);
+    printf("the value of a %d\n", a);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 11;
+    b = ++a;
+    printf("the value of b %d\n", b);
+    printf("the value of a %d\n", a);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 11;
+    b = a--;
+    printf("the value of b %d\n", b);
+    printf("the value of a %d\n", a);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    int b = 11;
+    b *= a;
+    printf("the value of b %d\n", b);
+    printf("the value of a %d\n", a);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a;
+    a = 2 % 5 + (8 / 4) + 20 - 5;
+    printf("%d", a);
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main()
+{
+    int a = 11;
+    printf("%d\n", a++ == 12 || a == 12);
+    printf("\n%d", a);
+    return 0;
+}
+*/
